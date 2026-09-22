aa779435bb37268c405c3d519139ff42ba4a31dc
MalekAbdallah <maleka28@nycstudents.net>
2026-09-22 19:35:39 +0000

    aa

---
+++ Malek Abdallah - Lesson-03b-Classwork/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    int poofy =-5;
+    int elmo = 10;
+
+    double z = poofy + elmo;
+    Systems.out.println("z");
+   
+    
+  }
+
+  
+}
